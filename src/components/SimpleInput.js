5d172f156f605cfef73590bffd473298c0863b56
ProfessorBunny <nikunjraghav24@gmail.com>
2023-06-10 11:43:27 +0100

    Adding input validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,63 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [inputIsValid, setInputIsValid] = useState(false);
+  const [formIstouched, setFormIsTouched] = useState(false);
+
+  const nameInputRef = useRef();
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setInputIsValid(true);
+    }
+  };
+
+  const inputBlurHandler = () => {
+    setFormIsTouched(true);
+    if (enteredName.trim() === "") {
+      setInputIsValid(false);
+    }
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setFormIsTouched(true);
+    if (enteredName.trim() === "") {
+      setInputIsValid(false);
+      return;
+    }
+    setInputIsValid(true);
+
+    console.log("input from reading change with state: " + enteredName);
+    const enteredNameRef = nameInputRef.current.value;
+    console.log("input from reading value with ref: " + enteredNameRef);
+
+    // nameInputRef.current.value = "";
+
+    setEnteredName(" ");
+  };
+  const nameInputIsValid = formIstouched && !inputIsValid;
+  const formClasses = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={formClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          onChange={nameChangeHandler}
+          onBlur={inputBlurHandler}
+          value={enteredName}
+          type="text"
+          id="name"
+        />
+        {nameInputIsValid && (
+          <p className="error-text">Please enter a valid input</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
